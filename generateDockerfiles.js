97e1430da915733567ab1465ea41fe0112bc2e0c
Evan Black <evan@casestack.io>
2018-01-09 13:02:21 -0600

    Make fixes

--- generateDockerfiles.js
+++ generateDockerfiles.js
@@ -1,13 +1,23 @@
-const fs = require('fs');
-const mustache = require('mustache');
-const config = require('./config.json');
+function run() {
+  const fs = require('fs');
+  const mustache = require('mustache');
+  const config = require('./config.json');
 
-const templateFile = fs.readFileSync('Dockerfile.template', 'utf8');
-mustache.parse(templateFile);
+  const template = fs.readFileSync('Dockerfile.template', 'utf8');
+  mustache.parse(template);
 
-for (const dockerfile of config.dockerfiles) {
-  const rendered = mustache.render(templateFile, dockerfile);
-  const name = `Dockerfile.${dockerfile.tag}`
-  fs.writeFileSync(name, rendered, { encoding: 'utf8' });
-  console.log(`Generated ${name}`);
+  for (const dockerfile of config.dockerfiles) {
+    const view = {
+      ...dockerfile,
+      anyInstructions: !!dockerfile.extraInstructions
+    };
+    const rendered = mustache.render(template, view);
+    const name = `out/Dockerfile.${dockerfile.tag}`
+    fs.writeFileSync(name, rendered, { encoding: 'utf8' });
+    console.log(`Generated ${name}`);
+  }
+}
+
+if (require.main === module) {
+  run();
 }
