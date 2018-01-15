960c6d3c6208f02d7afa92eb97522a9d49d0eacd
Evan Black <evan@erblack.com>
2018-01-15 14:46:21 -0600

    Add generation of Dockerfiles based on a template (#3)
    * Add generation of Dockerfiles based on a template
    
    * Make fixes
    
    * Move config info out of code block
    
    * Move requires to top

---
+++ generateDockerfiles.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const mustache = require('mustache');
+const config = require('./config.json');
+
+function run() {
+  const template = fs.readFileSync('Dockerfile.template', 'utf8');
+  mustache.parse(template);
+
+  for (const dockerfile of config.dockerfiles) {
+    const view = {
+      ...dockerfile,
+      anyInstructions: !!dockerfile.extraInstructions
+    };
+    const rendered = mustache.render(template, view);
+    const name = `out/Dockerfile.${dockerfile.tag}`
+    fs.writeFileSync(name, rendered, { encoding: 'utf8' });
+    console.log(`Generated ${name}`);
+  }
+}
+
+if (require.main === module) {
+  run();
+}
